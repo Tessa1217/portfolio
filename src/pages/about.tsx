70e019479d606087eb19a7657975263e8f599afe
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-23 00:30:14 +0900

    update pages wrapper with motion wrapper

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,26 +1,25 @@
-import { motion } from "framer-motion";
-import { fadeInUp } from "../constants/animation/animation";
-import { useInViewAnimation } from "../hooks/useInViewAnimation";
-import QuestionCard from "../components/question-card";
-import { QUESTION_ANS_ANSWER } from "../lib/data";
-import SectionTitle from "../components/section-title";
+import { fadeInUp } from "@/constants/animation/animation";
+import { QUESTION_ANS_ANSWER } from "@/lib/data";
+import QuestionCard from "@/components/question-card";
+import SectionTitle from "@/components/section-title";
+import MotionWrapper from "@/components/motion-wrapper";
 
 export default function About() {
-  const { ref, controls } = useInViewAnimation();
   return (
     <section className="h-screen flex items-center justify-center text-white">
-      <motion.div
-        ref={ref}
-        initial="hidden"
-        animate={controls}
+      <MotionWrapper
         variants={fadeInUp}
         className="text-center px-4 w-screen p-10"
       >
         <SectionTitle title="About Me" />
         {QUESTION_ANS_ANSWER.map((qna) => (
-          <QuestionCard question={qna.question} answer={qna.answer} />
+          <QuestionCard
+            key={qna.key}
+            question={qna.question}
+            answer={qna.answer}
+          />
         ))}
-      </motion.div>
+      </MotionWrapper>
     </section>
   );
 }
