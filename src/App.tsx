ee6d10649689055a36f0341482dec9eaea216c13
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-25 01:43:14 +0900

    update app page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
 import Introduction from "./pages/introduction";
 import About from "./pages/about";
 import Skills from "./pages/skills";
-import { ScrollProgress } from "./components/scroll-progress";
+import Experience from "@/pages/experience";
+import AnimatedProgressScroll from "@/components/animation/animated-progress-scroll";
+import Navbar from "@/components/layout/navbar";
+import AnimatedEffect from "@/components/animation/animated-effect";
 function App() {
   return (
     <>
       <div className="relative">
-        <div className="fixed inset-0 z-0 bg-animated-gradient pointer-events-none" />
-        <ScrollProgress />
+        <Navbar />
+        <div className="fixed inset-0 z-0 pointer-events-none bg-animated-gradient" />
+        <div className="fixed w-full h-screen">
+          <AnimatedEffect count={100} minSize={2} maxSize={8} />
+        </div>
+        {/* <HeroBackground /> */}
+        <AnimatedProgressScroll />
         <div className="relative z-10">
           <Introduction />
           <About />
+          <Experience />
           <Skills />
         </div>
       </div>
