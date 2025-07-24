62b49b2730b827cfbe6231a8cdd780296b17f8d7
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-25 01:43:40 +0900

    add hero illustration component for web main icon image

---
+++ src/components/hero-illustration.tsx
@@ -0,0 +1,11 @@
+export default function HeroIllustration() {
+  return (
+    <div className="rounded-full overflow-hidden">
+      <img
+        src="/illustration/developer-icon.png"
+        alt="Developer Main Icon"
+        className="w-full h-full object-cover"
+      />
+    </div>
+  );
+}
