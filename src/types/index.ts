bfbdf9f4e544cdfe69e0d3414deaa747714f1cd0
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-25 01:42:16 +0900

    add work information type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,12 @@ export interface QuestionAnswer {
   question: string;
   answer: string;
 }
+
+export interface WorkInformation {
+  id: number;
+  company: string;
+  role: string;
+  period: string;
+  logoUrl: string;
+  description?: string;
+}
