a8573d4c2618a28608b9868407251d5fb41ac79f
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-26 03:35:59 +0900

    fix background for particles

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,15 @@ import Skills from "./pages/skills";
 import Experience from "@/pages/experience";
 import AnimatedProgressScroll from "@/components/animation/animated-progress-scroll";
 import Navbar from "@/components/layout/navbar";
-import AnimatedEffect from "@/components/animation/animated-effect";
+import AnimatedParticles from "@/components/animation/animated-particles";
 function App() {
   return (
     <>
       <div className="relative">
         <Navbar />
-        <div className="fixed inset-0 z-0 pointer-events-none bg-animated-gradient" />
+        <div className="fixed inset-0 z-0 pointer-events-none bg-[#15182e]" />
         <div className="fixed w-full h-screen">
-          <AnimatedEffect count={100} minSize={2} maxSize={8} />
+          <AnimatedParticles count={25} minSize={5} maxSize={60} />
         </div>
         {/* <HeroBackground /> */}
         <AnimatedProgressScroll />
