6da6fbebea64803c441db0a4a546858c5607e1fd
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-26 03:35:12 +0900

    fix animation variants

--- src/constants/animation/animation.ts
+++ src/constants/animation/animation.ts
@@ -60,19 +60,40 @@ export const textContainerVariants: Variants = {
   visible: {
     transition: {
       staggerChildren: 0.05,
-      delayChildren: 0.3,
+      delayChildren: 1,
     },
   },
   exit: {
     transition: {
       staggerChildren: 0.03,
-      staggerDirection: -1,
+      staggerDirection: 0.7,
     },
   },
 };
 
 export const textVariants: Variants = {
-  hidden: { opacity: 0, y: 20 },
-  visible: { opacity: 1, y: 0 },
-  exit: { opacity: 0, y: -20 },
+  // hidden: { opacity: 0, y: 0 },
+  // visible: (i: number) => ({
+  //   opacity: 1,
+  //   y: 0,
+  //   transition: {
+  //     delay: i * 0.05,
+  //   },
+  // }),
+  // exit: { opacity: 0, y: -10, transition: { duration: 0.2 } },
+  hidden: { opacity: 0 },
+  visible: { opacity: 1, transition: { opacity: { duration: 0 } } },
+};
+
+export const cursorVariants: Variants = {
+  blinking: {
+    opacity: [0, 0, 1, 1],
+    transition: {
+      duration: 1,
+      repeat: Infinity,
+      repeatDelay: 0,
+      ease: "linear",
+      times: [0, 0.5, 0.5, 1],
+    },
+  },
 };
