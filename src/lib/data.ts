68831a07cac2c959bc2e44d5e2c5563b52c6183e
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-28 05:01:46 +0900

    update data for display

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,3 +1,5 @@
+import type { SkillInformation } from "@/types";
+
 export const HERO_IDENTIFICATION = ["YuJin Kwon", "권유진"];
 export const HERO_TEXT = [
   "3년차 열정러",
@@ -10,7 +12,7 @@ export const QUESTION_ANS_ANSWER = [
     key: "question_1",
     question: "프론트엔드 개발을 시작하게 된 계기는 무엇인가요?",
     answer:
-      "저는 사용자의 행동과 심리를 분석하는 데 관심이 많았고, 그것을 시작적이고 직관적으로 구현할 수 있는 프론트엔드에 매력을 느꼈어요.",
+      "프론트엔드는 사용자의 반응을 가장 먼저 만날 수 있어서, 제 일에 대한 피드백이 즉각적으로 오는 게 무척 보람됐습니다. 계속 변화하는 기술 흐름을 따라가며 공부하고 다양한 시도와 경험을 할 수 있다는 점도 저의 도전적이고 열정적인 성향과 잘 맞았습니다. 그래서 망설임 없이 프론트엔드를 선택하게 되었습니다!",
   },
   {
     key: "question_2",
@@ -34,14 +36,14 @@ export const WORK_EXPERIENCE = [
     period: "2022.10 - Present",
     description:
       "Vue.js 기반 교육·공공 서비스 UI/UX 최적화, 성능 개선 프로젝트 진행",
-    logoUrl: "/logo/Vue.js.png",
+    logoUrl: "/logo/Company_1_logo.png",
     projects: [
       {
         id: 1,
         title: "국립대구과학관 홈페이지 개편",
         period: "2025.02-2025.05",
         description:
-          "국립대구과학관 사용자 및 관리자 서비스를 Vue.js 3 기반 프론트엔드와 Spring boot 기반 백엔드로 통합하여 클라우드 환경에 맞춰 고도화를 진행했습니다.",
+          "Vue.js 3 기반 프론트엔드와 Spring boot 기반의 사용자/관리자 웹 서비스 개편",
         tags: [
           "Vue.js",
           "Pinia",
@@ -61,7 +63,7 @@ export const WORK_EXPERIENCE = [
         title: "에듀넷 티-클리어 클라우드 전환 및 서비스 재구조화",
         period: "2024.08-2025.02",
         description:
-          "Legacy 기반으로 운영되던 에듀넷 사용자 서비스를 포함한 6개의 서비스를 클라우드 환경에 맞게 구조를 재설계 및 통합 마이그레이션을 진행했습니다.",
+          "Legacy 기반으로 운영되던 에듀넷 사용자 서비스를 포함한 6개의 서비스를 클라우드 환경에 맞게 구조를 재설계 및 통합 마이그레이션",
         tags: [
           "Vue.js",
           "Pinia",
@@ -83,7 +85,7 @@ export const WORK_EXPERIENCE = [
         title: "사내 관리자 프레임워크 개발",
         period: "2024.01-2024.07",
         description:
-          "사내 프로젝트의 관리자(Admin) 공통 기능을 분석하여 다양한 서비스에 일관된 기능을 제공할 수 있도록 재사용 가능한 관리자 Framework 구축",
+          "사내 프로젝트의 관리자(Admin) 공통 기능을 분석하여 관리자 Framework 구축",
         tags: [
           "Vue.js",
           "Pinia",
@@ -118,59 +120,70 @@ export const PERSONAL_PROJECT_EXPERIENCE = [
   },
 ];
 
-export const SKILL_SET = {
+export const SKILL_SET: Record<string, SkillInformation[]> = {
   frontend: [
     {
-      name: "JavaScript",
-      icon: "/logo/JavaScript.png",
+      id: 1,
+      skillName: "JavaScript",
+      skillIconUrl: "/logo/JavaScript.png",
     },
     {
-      name: "Vue.js",
-      icon: "/logo/Vue.js.png",
+      id: 2,
+      skillName: "Vue.js",
+      skillIconUrl: "/logo/Vue.js.png",
     },
     {
-      name: "React",
-      icon: "/logo/React.png",
+      id: 3,
+      skillName: "React",
+      skillIconUrl: "/logo/React.png",
     },
     {
-      name: "Tailwind CSS",
-      icon: "/logo/Tailwind CSS.png",
+      id: 4,
+      skillName: "Tailwind CSS",
+      skillIconUrl: "/logo/Tailwind CSS.png",
     },
     {
-      name: "TypeScript",
-      icon: "/logo/TypeScript.png",
+      id: 5,
+      skillName: "TypeScript",
+      skillIconUrl: "/logo/TypeScript.png",
     },
   ],
   backend: [
     {
-      name: "Java",
-      icon: "/logo/Java.png",
+      id: 1,
+      skillName: "Java",
+      skillIconUrl: "/logo/Java.png",
     },
   ],
   database: [
     {
-      name: "MySQL",
-      icon: "/logo/MySQL.png",
+      id: 1,
+      skillName: "MySQL",
+      skillIconUrl: "/logo/MySQL.png",
     },
     {
-      name: "PostgresSQL",
-      icon: "/logo/PostgresSQL.png",
+      id: 2,
+      skillName: "PostgresSQL",
+      skillIconUrl: "/logo/PostgresSQL.png",
     },
   ],
   design: [
     {
-      name: "Figma",
-      icon: "/logo/Figma.png",
+      id: 1,
+      skillName: "Figma",
+      skillIconUrl: "/logo/Figma.png",
     },
   ],
   build: [
     {
-      name: "Vite",
-      icon: "/logo/Vite.js.png",
+      id: 1,
+      skillName: "Vite",
+      skillIconUrl: "/logo/Vite.js.png",
     },
     {
-      name: "Gradle",
-      icon: "/logo/Gradle.png",
+      id: 2,
+      skillName: "Gradle",
+      skillIconUrl: "/logo/Gradle.png",
     },
   ],
 };
