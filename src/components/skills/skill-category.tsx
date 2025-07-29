5285e3301617a9af2d654daa4a2f7002c0d7cba5
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-30 03:32:02 +0900

    replace skill category with animated components

--- src/components/skills/skill-category.tsx
+++ src/components/skills/skill-category.tsx
@@ -1,7 +1,6 @@
 import SkillCard from "@/components/skills/skill-card";
-import { fadeSlideUp } from "@/constants/animation/animation";
 import { type SkillInformation } from "@/types";
-import { motion } from "framer-motion";
+import { AnimatedCard, AnimatedElement } from "@/components/animation";
 
 interface SkillCategoryProps {
   categoryName: string;
@@ -13,18 +12,20 @@ export default function SkillCategory({
   skills,
 }: SkillCategoryProps) {
   return (
-    <motion.div
-      variants={fadeSlideUp}
-      className="bg-gray-800 rounded-2xl p-6 flex flex-col h-full"
-    >
-      <h3 className="text-2xl uppercase font-header mb-8 text-white">
-        {categoryName}
-      </h3>
-      <div className="flex flex-wrap gap-4">
-        {skills.map((skill) => (
-          <SkillCard key={`${categoryName}_${skill.id}`} {...skill} />
-        ))}
-      </div>
-    </motion.div>
+    <AnimatedElement listChildren={true} animation="fadeInUp">
+      <AnimatedCard
+        hoverEffect="scale"
+        className="bg-gray-800 rounded-2xl p-6 flex flex-col h-full"
+      >
+        <h3 className="text-2xl uppercase font-header mb-8 text-white">
+          {categoryName}
+        </h3>
+        <div className="flex flex-wrap gap-4">
+          {skills.map((skill) => (
+            <SkillCard key={`${categoryName}_${skill.id}`} {...skill} />
+          ))}
+        </div>
+      </AnimatedCard>
+    </AnimatedElement>
   );
 }
