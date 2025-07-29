83c41e05931b6d260a50c774cb36b21840bb1134
Tessa1217 <kwonyuj1996@gmail.com>
2025-07-30 03:32:39 +0900

    add index.ts for animation components

---
+++ src/components/animation/index.ts
@@ -0,0 +1,18 @@
+// Animation components
+export { default as AnimatedSection } from "@/components/animation/animated-section";
+export { default as AnimatedCard } from "@/components/animation/animated-card";
+export { default as AnimatedList } from "@/components/animation/animated-list";
+export { default as AnimatedText } from "@/components/animation/animated-text";
+export { default as AnimatedElement } from "@/components/animation/animated-element";
+export { default as AnimatedEffect } from "@/components/animation/animated-effect";
+export { default as AnimatedParticles } from "@/components/animation/animated-particles";
+export { default as AnimatedProgressScroll } from "@/components/animation/animated-progress-scroll";
+
+// Animation 훅
+export {
+  useInViewAnimation,
+  useCardAnimation,
+  useListAnimation,
+  useHeroAnimation,
+  useContinuousAnimation,
+} from "@/hooks/useInViewAnimation";
